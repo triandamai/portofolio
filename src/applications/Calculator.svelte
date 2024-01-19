9ae63b2524a1ceabcd856644d3d9b4ee5b5cfa1b
trian damai <triannurizkillah@gmail.com>
2024-01-19 15:52:43 +0700

    feat: working on calculator

--- src/applications/Calculator.svelte
+++ src/applications/Calculator.svelte
@@ -3,8 +3,15 @@
 	import TrafficLight from '../components/systemUI/TrafficLight.svelte';
 	import { closeApplication, minimizeApplication, maximizeApplication } from '$lib/kernel/kernel';
 	import type { Application, OptionsMenu } from '$lib/kernel/type';
-	import { onMount } from 'svelte';
+	import { SvelteComponent, type ComponentType, onMount } from 'svelte';
 	import { registerReceiver, unregisterReceiver } from '$lib/kernel/application/application';
+
+	import Divide from '../components/SVG/icons/Divide.svelte';
+	import Plus from '../components/SVG/icons/Plus.svelte';
+	import Minus from '../components/SVG/icons/Minus.svelte';
+	import Multiply from '../components/SVG/icons/Multiply.svelte';
+	import PlusSlashMinus from '../components/SVG/icons/PlusSlashMinus.svelte';
+	import Equal from '../components/SVG/icons/Equal.svelte';
 
 	export let width: number;
 	export let height: number;
@@ -14,11 +21,12 @@
 	const buttonSize = width / 4;
 
 	type button = {
-		operator: '+' | '-' | '=' | '/' | '*' | ',' | '%' | 'ac' | 'btn';
+		operator: '+' | '-' | '=' | '/'|'-+' | '*' | ',' | '%' | 'ac' | 'btn';
 		text: string;
 		value: string;
 		scale: number;
 		color: string;
+		component: ComponentType<SvelteComponent> | undefined;
 	};
 
 	const button: Array<Array<button>> = [
@@ -28,28 +36,32 @@
 				text: 'AC',
 				value: '',
 				scale: 1,
-				color: 'bg-gray-700'
+				color: 'bg-gray-700',
+				component: undefined
 			},
 			{
-				operator: '/',
+				operator: '-+',
 				text: '/',
 				value: '',
 				scale: 1,
-				color: 'bg-gray-700'
+				color: 'bg-gray-700',
+				component: PlusSlashMinus
 			},
 			{
 				operator: '%',
 				text: '%',
 				value: '',
 				scale: 1,
-				color: 'bg-gray-700'
+				color: 'bg-gray-700',
+				component: undefined
 			},
 			{
 				operator: '/',
 				text: '/',
 				value: '',
 				scale: 1,
-				color: 'bg-amber-500'
+				color: 'bg-amber-500',
+				component: Divide
 			}
 		],
 		[
@@ -58,28 +70,32 @@
 				text: '7',
 				value: '7',
 				scale: 1,
-				color: 'bg-gray-400'
+				color: 'bg-gray-400',
+				component: undefined
 			},
 			{
 				operator: 'btn',
 				text: '8',
 				value: '8',
 				scale: 1,
-				color: 'bg-gray-400'
+				color: 'bg-gray-400',
+				component: undefined
 			},
 			{
 				operator: 'btn',
 				text: '9',
 				value: '9',
 				scale: 1,
-				color: 'bg-gray-400'
+				color: 'bg-gray-400',
+				component: undefined
 			},
 			{
 				operator: '*',
 				text: 'x',
 				value: '',
 				scale: 1,
-				color: 'bg-amber-500'
+				color: 'bg-amber-500',
+				component: Multiply
 			}
 		],
 		[
@@ -88,28 +104,32 @@
 				text: '4',
 				value: '4',
 				scale: 1,
-				color: 'bg-gray-400'
+				color: 'bg-gray-400',
+				component: undefined
 			},
 			{
 				operator: 'btn',
 				text: '5',
 				value: '5',
 				scale: 1,
-				color: 'bg-gray-400'
+				color: 'bg-gray-400',
+				component: undefined
 			},
 			{
 				operator: 'btn',
 				text: '6',
 				value: '6',
 				scale: 1,
-				color: 'bg-gray-400'
+				color: 'bg-gray-400',
+				component: undefined
 			},
 			{
 				operator: '-',
 				text: '-',
 				value: '',
 				scale: 1,
-				color: 'bg-amber-500'
+				color: 'bg-amber-500',
+				component: Minus
 			}
 		],
 		[
@@ -118,62 +138,73 @@
 				text: '1',
 				value: '1',
 				scale: 1,
-				color: 'bg-gray-400'
+				color: 'bg-gray-400',
+				component: undefined
 			},
 			{
 				operator: 'btn',
 				text: '2',
 				value: '2',
 				scale: 1,
-				color: 'bg-gray-400'
+				color: 'bg-gray-400',
+				component: undefined
 			},
 			{
 				operator: 'btn',
 				text: '3',
 				value: '3',
 				scale: 1,
-				color: 'bg-gray-400'
+				color: 'bg-gray-400',
+				component: undefined
 			},
 			{
 				operator: '+',
 				text: '+',
 				value: '',
 				scale: 1,
-				color: 'bg-amber-500'
+				color: 'bg-amber-500',
+				component: Plus
 			}
 		],
 		[
 			{
-				operator: '+',
+				operator: 'btn',
 				text: '0',
-				value: '',
+				value: '0',
 				scale: 2,
-				color: 'bg-gray-400 rounded-bl-md'
+				color: 'bg-gray-400 rounded-bl-md',
+				component: undefined
 			},
 			{
 				operator: ',',
 				text: ',',
 				value: '',
 				scale: 1,
-				color: 'bg-gray-400'
+				color: 'bg-gray-400',
+				component: undefined
 			},
 			{
 				operator: '=',
 				text: '=',
 				value: '',
 				scale: 1,
-				color: 'bg-amber-500 rounded-br-md'
+				color: 'bg-amber-500 rounded-br-md',
+				component: Equal
 			}
 		]
 	];
 
-	function calculateSizeForZeroButton(buttonSize:number,colIndex:number,rowIndex:number):number{
-		if(colIndex < button.length-1) return buttonSize
+	function calculateSizeForZeroButton(
+		buttonSize: number,
+		colIndex: number,
+		rowIndex: number
+	): number {
+		if (colIndex < button.length - 1) return buttonSize;
 
-		if(rowIndex == 0){
-			return buttonSize + 0.5
+		if (rowIndex == 0) {
+			return buttonSize + 0.5;
 		}
-		return buttonSize
+		return buttonSize;
 	}
 
 	onMount(() => {
@@ -219,9 +250,16 @@
 				<div class="w-full flex flex-row place-content-center">
 					{#each btn as b, rowIdx}
 						<button
-							style="width: {calculateSizeForZeroButton(buttonSize,index,rowIdx) * b.scale}px; height:{buttonSize - (buttonSize / 5)}px;"
-							class="w-10 h-10 text-white  focus:bg-opacity-60 {b.color}">{b.text}</button
+							style="width: {calculateSizeForZeroButton(buttonSize, index, rowIdx) *
+								b.scale}px; height:{buttonSize - buttonSize / 5}px;"
+							class="w-10 h-10 text-white font-sf-bold text-md place-content-center flex flex-row focus:bg-opacity-60 {b.color}"
 						>
+							{#if b.component}
+								<svelte:component this={b.component} class="w-7 h-7 place-self-center" />
+							{:else}
+								<span class="place-self-center">{b.text}</span>
+							{/if}
+						</button>
 						{#if rowIdx < btn.length - 1}
 							<div class="w-[1px] h-full bg-gray-900" />
 						{/if}
